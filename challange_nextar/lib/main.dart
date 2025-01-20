b0e521111207e81ffa954ee585c3e1fac207fabf
Natan-Saicon <dev1@saicon.com.br>
2025-01-20 17:53:12 -0300

    Configuração do Firebase no projeto

--- challange_nextar/lib/main.dart
+++ challange_nextar/lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:challange_nextar/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
+
+  FirebaseFirestore.instance.collection('teste').add({'teste': 'teste'});
+
 }
 
 class MyApp extends StatelessWidget {
