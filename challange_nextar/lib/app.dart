0ead4e971891bd1433ff277764d54b6d62fbdc79
Natan Peliciolli Loss <natanpeliciolliloss@gmail.com>
2025-01-27 00:48:38 -0300

    Ajuste da inicialização do aplicativo

--- challange_nextar/lib/app.dart
+++ challange_nextar/lib/app.dart
@@ -1,13 +1,13 @@
+import 'package:challange_nextar/routes/pages.dart';
 import 'package:challange_nextar/routes/routes.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
-
 import 'package:challange_nextar/utils/colors.dart';
 import 'package:challange_nextar/utils/images.dart';
 import 'package:challange_nextar/viewmodels/forgot_password_viewmodel.dart';
+import 'package:challange_nextar/viewmodels/home_viewmodel.dart';
 import 'package:challange_nextar/viewmodels/login_viewmodel.dart';
 import 'package:challange_nextar/viewmodels/client_viewmodel.dart';
 import 'package:challange_nextar/viewmodels/products_viewmodel.dart';
-import 'package:challange_nextar/views/account/login_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -31,6 +31,9 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(
           create: (_) => ProductViewModel(),
         ),
+        ChangeNotifierProvider(
+          create: (_) => HomeViewModel(),
+        ),
       ],
       child: MaterialApp(
         title: 'Challenger Nextar',
@@ -38,7 +41,7 @@ class MyApp extends StatelessWidget {
         onGenerateRoute: Pages.generateRoute,
         home: AnimatedSplashScreen(
           splash: AppImages.splashGif,
-          nextScreen: const LoginView(),
+          nextScreen: const SplashHandler(),
           splashIconSize: 1500,
           centered: true,
           duration: 2900,
@@ -51,6 +54,42 @@ class MyApp extends StatelessWidget {
           ),
           dividerTheme: const DividerThemeData(color: Colors.black),
         ),
+      ),
+    );
+  }
+}
+
+class SplashHandler extends StatefulWidget {
+  const SplashHandler({super.key});
+
+  @override
+  State<SplashHandler> createState() => _SplashHandlerState();
+}
+
+class _SplashHandlerState extends State<SplashHandler> {
+  late LoginViewModel loginViewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    loginViewModel = Provider.of<LoginViewModel>(context, listen: false);
+    _checkLoginStatus();
+  }
+
+  Future<void> _checkLoginStatus() async {
+    await loginViewModel.loadCurrentUser();
+    if (loginViewModel.currentUser != null) {
+      Navigator.pushReplacementNamed(context, Routes.hiddenDrawer);
+    } else {
+      Navigator.pushReplacementNamed(context, Routes.login);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(color: AppColors.primary),
       ),
     );
   }
